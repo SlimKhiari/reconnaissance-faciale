397babfdd294d0c8d66d978496afda49987923e9
Slim Khiari <73532355+SlimKhiari@users.noreply.github.com>
2022-01-13 03:57:49 +0100

    Add files via upload

--- reconnaissance.py
+++ reconnaissance.py
@@ -1,23 +1,25 @@
-
 import face_recognition
 import cv2
 import numpy as np
 import os
 import glob
-
-#IMPORTER DES IMAGES 
-print("-- importation des images des images --")
+import time 
+#importation des images
+print("-etape 1- importation des images des images --")
 faces_encodings = []
 faces_names = []
-cur_direc = os.getcwd()
+#getcwd() : pour obtenir le repertoire courant
+cur_direc = os.getcwd() 
 path = os.path.join(cur_direc, 'faces/')
 list_of_files = [f for f in glob.glob(path+'*.jpg')]
 number_files = len(list_of_files)
 names = list_of_files.copy()
 print("-- opération réussie --")
+print("#################################################################################################################################")
 
 #FORMER LES VISAGES
-print("-- formation des images --")
+#entrainement des images 
+print("-etape 2- formation des images --")
 for i in range(number_files):
     globals()['image_{}'.format(i)] = face_recognition.load_image_file(list_of_files[i])
     globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
@@ -26,72 +28,84 @@ for i in range(number_files):
     names[i] = names[i].replace(cur_direc, "")  
     faces_names.append(names[i])
 print("-- opération réussie --")
-
+print("#################################################################################################################################")
 
 #RECONNAISSANCE DE VISAGE
-print("-- reconnaissance des visages --")
+print("-etape 3- reconnaissance des visages --")
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
 print("-- opération réussie --")
+print("#################################################################################################################################")
 
-print("-- ouverture de la caméra --")
+print("-etape 4- ouverture de la caméra --")
 video_capture = cv2.VideoCapture(0)
 print("-- opération réussie --")
+print("#################################################################################################################################")
 
 while True:
 
 	#saisir une seule image video
 	ret, frame = video_capture.read()
 	
-	#resize frame of video to 1/4 for faster face recoginition processing
+	#redimensionner l'image de la vidéo à 1/4 pour un traitement de reconnaissance faciale plus rapide
 	small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 	
-	#de BGR color which OpenCV uses à RGB color  which face_recognition uses
+	#de la couleur BGR utilisée par OpenCV à la couleur RVB utilisée par face_recognition
 	rgb_small_frame = small_frame[:, :, ::-1]
 	
 	
 	if process_this_frame:
-			#find all the faces and face encodings in the current frame of video	
+			#trouver tous les visages et encodages de visage dans l'image actuelle de la vidéo
 			face_locations = face_recognition.face_locations( rgb_small_frame)
 			face_encodings = face_recognition.face_encodings( rgb_small_frame, face_locations)
 			
 			face_names = []
 			for face_encoding in face_encodings:
-				#see if the face is a match for the current frame of video
+				#voir si le visage correspond à l'image actuelle de la vidéo
 				matches = face_recognition.compare_faces (faces_encodings, face_encoding)
-				name = "Personne inconnue"
+				name = "refuse"
 			
 				face_distances = face_recognition.face_distance( faces_encodings, face_encoding)
 				best_match_index = np.argmin(face_distances)
 				if matches[best_match_index]:
+					print("*************************************************");
+					print("Personne autorisée à entrer dans le batiment.");
+					print("Ouverture automatique des portes, veuillez entrer >");
+					#time.sleep(5)
+					print("Fermeture automatique des portes.");
+					print("*************************************************\n\n");
 					name = faces_names[best_match_index]
-				
+				else:	
+					print("*************************************************");
+					print("Personne non autorisée à entrer dans le batiment, portes bloquées.");
+					print("*************************************************\n\n");
+					#time.sleep(5)
 				face_names.append(name)
 			
 	process_this_frame = not process_this_frame
 	
-	# Display the results
+	# affichage des résultats
 	for (top, right, bottom, left), name in zip(face_locations, face_names):
-		#scale back up locations since the frame we detected in was scaled to 1/4 size
+		# mettre à l'échelle les emplacements de sauvegarde puisque la trame dans laquelle nous avons détecté a été mise à l'échelle à 1/4 de taille
 		top *= 4
 		right *= 4
 		bottom *= 4
 		left *= 4
 			
-		# Draw a rectangle around the face
+		# tracer un carré au tour du visage
 		cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 5)
 		
-		# Input text label with a name below the face
+		# étiquette de texte d'entrée avec un nom sous le visage
 		cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
 		font = cv2.FONT_HERSHEY_DUPLEX
 		cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 		
-	# Display the resulting image
+	# afficher l'image résultante
 	cv2.imshow('Video', frame)
 	
-	# Hit 'q' on the keyboard to quit!
+	# Appuyez sur « q » sur le clavier pour quitter !
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 			
